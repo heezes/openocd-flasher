80b3663dbe403d3a68849ef507de9bb24c35db28
Altamash <warlock.unleash@gmail.com>
2021-02-25 12:29:00 +0530

    Major Changes: ->Added a background rtt logger script ->Adding a background trigger shell script ->modifying current script for triggering

--- test.py
+++ test.py
@@ -1,42 +1,44 @@
 import time
 import socket
 import threading
-try:
-    from gpiozero import LED, Button
-except Exception as e:
-    print(e.args)
-
+import logging
+from logging.handlers import RotatingFileHandler
+import sys
 
 def startRttServer():
+    logger = logging.getLogger('test.py')
+    logger.setLevel(logging.DEBUG)
+    # create a file e
+    handler = RotatingFileHandler('rtt.log', maxBytes=1*1024*1024, backupCount=1)
+    handler.setLevel(logging.DEBUG)
+
+    # # create a logging format
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    # add the file handler to the logger
+    logger.addHandler(handler)
+
     port = 9090 # socket server port number
     host = "127.0.0.1"
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
     while True:
-        time.sleep(0.2)
-        data = client_socket.recv(1024).decode()  # receive response
-        if(len(data) > 0):
-            print(data)
+        try:
+            time.sleep(0.2)
+            data = client_socket.recv(1024).decode()  # receive response
+            if(len(data) > 0):
+                try:
+                    logger.debug(data)
+                except Exception as e:
+                    print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
+                    logger.debug(e)
+        except Exception as e:
+            logger.debug(e)
+            client_socket.close()  # close the connection
+            client_socket.connect((host, port))  # connect to the server
 
-    client_socket.close()  # close the connection
 
 
 
 x = threading.Thread(target = startRttServer)
 x.start()
-try:
-    key_pin = LED(6)
-except Exception as e:
-   print(e)
-while(True):
-    time.sleep(1)
-    case = int(input("Run Test Case: "))
-    if case == 1:
-        key_pin.on()
-        print("Unlocking")
-    elif case == 2:
-        key_pin.off()
-        print("Locking")
-    else:
-        pass
-    case = 0
--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sudo openocd -f debug.cfg &
 sudo python3 test.py &
